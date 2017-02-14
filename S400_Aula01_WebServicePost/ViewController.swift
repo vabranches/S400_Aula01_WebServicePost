265fbad65df8d20d89e43cd51c1b43e5cf8182e1
Swift <swift@valter.local>
2017-02-14 21:03:36 -0200

    final

--- S400_Aula01_WebServicePost/ViewController.swift
+++ S400_Aula01_WebServicePost/ViewController.swift
@@ -10,7 +10,9 @@ class ViewController: UIViewController {
     
     //MARK: Propriedades
     var minhaSessao : URLSession!
-    var baseURL = URL(string: "https://meuws.herokuapp.com/api/v1/jogos")
+    var baseURL = URL(string: "https://meuws.herokuapp.com/api/v1/jogos")!
+
+    
     
 
     override func viewDidLoad() {
@@ -23,8 +25,67 @@ class ViewController: UIViewController {
     
     //MARK: Actions
     @IBAction func cadastrar(_ sender: UIButton) {
-        var minhaTarefa = minhaSessao.dataTask(with: baseURL!) { (data, response, erro) in
-            //
+        var stringTitulo = ""
+        var stringPlataforma = ""
+        var stringAno = ""
+        
+
+            
+            if !(tituloTextField.text!.isEmpty || plataformaTextField.text!.isEmpty || anoTextField.text!.isEmpty) {
+                stringTitulo = self.tituloTextField.text!
+                stringPlataforma = self.plataformaTextField.text!
+                stringAno = self.anoTextField.text!
+                
+                //A tarefa post necessita de uma requisicao
+                var minhaRequisicao = URLRequest(url: self.baseURL)
+                minhaRequisicao.httpMethod = "POST"
+                minhaRequisicao.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                
+                //Montar os dados que serão montados
+                let dadosUsuario = ["titulo" : stringTitulo, "plataforma" : stringPlataforma, "ano" : stringAno]
+                
+                do{
+                    let dadosUsuarioJSON = try JSONSerialization.data(withJSONObject: dadosUsuario, options: JSONSerialization.WritingOptions())
+                    minhaRequisicao.httpBody = dadosUsuarioJSON
+                } catch {}
+                
+                //Criando a tarefa que vai gravar o JSON
+                let tarefa = minhaSessao.dataTask(with: minhaRequisicao, completionHandler: { (data, response, erro) in
+                    
+                    if erro == nil {
+                        guard let meuResponse = response as? HTTPURLResponse else {return}
+                        
+                        if meuResponse.statusCode == 201{
+                            guard let meuData = data else {return}
+                            
+                            if let dataString = String(data: meuData, encoding: .utf8){
+                                print(dataString)
+                            }
+                            
+                            DispatchQueue.main.async {
+                                let alerta = UIAlertController(title: "Alerta", message: "Cadastro efetuado com sucesso", preferredStyle: .alert)
+                                let botao = UIAlertAction(title: "OK", style: .default)
+                                alerta.addAction(botao)
+                                self.present(alerta, animated: true)
+                                
+                                self.tituloTextField.text = ""
+                                self.plataformaTextField.text = ""
+                                self.anoTextField.text = ""
+                                
+                            }
+                            
+                        } else {
+                            print("\nStatus da rede: \(meuResponse.statusCode)\n")
+                        }
+                    }
+                })
+                
+                tarefa.resume()
+            } else {
+                let alerta = UIAlertController(title: "Alerta", message: "Preencha todos os campos", preferredStyle: .alert)
+                let botao = UIAlertAction(title: "OK", style: .default)
+                alerta.addAction(botao)
+                self.present(alerta, animated: true)
         }
     }
     
